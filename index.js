fe281f78936498bcdc9dcbee60d9706b045f8b70
Aida <edigeeva.aika@mail.ru>
2023-01-24 15:03:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let myName = "Aida";
+console.log(myName);
+let myLastName = "Edigeeva";
+console.log(myLastName);
+
+let myTown = "Karakol";
+console.log(myTown);
+
+let myStatus = "mama";
+console.log(myStatus);
+
+let ourCoutry = "Kyrgyzstan";
+console.log(ourCoutry);
+
+let myFavoriteBook = "Буть лучшей версией себя";
+console.log(myFavoriteBook);
+
+let myFavoriteFood = "шашлычок";
+console.log(myFavoriteFood);
+
+let myFavoriteFlower = "розы";
+let myFavoriteCar = "lamborginy";
+let myDream = "стать лучшим программистом";
+
+let myFavoriteNumber = 7;
+let myAge = 37;
+let myCarNumber = 6364;
+let homeNumber = 42;
+let inOurFfmily = 5;
+let itStudents = 17;
+let ourPopulation = 7000000;
+let regionKyrgyzstan = 7;
+let myHeight = 167;
+let theWeight = 60;
+
+let width = 40;
+let heght = 22;
+let length = 65;
+let volume = width * heght * length;
+console.log(volume);
+ 
+let girl = 12;
+let boy = 18;
+let students = girl + boy ;
+console.log(students);
+
+let flowers = 46;
+let women = 8;
+let bouquet = flowers / women;
+console.log(bouquet);
+
+let ex = (3*2) > 22;
+let ex1 = 11%5;
+console.log(ex1); 
+
